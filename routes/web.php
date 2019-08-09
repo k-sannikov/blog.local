b3ae190f35636eca171df310a93b0082c1b08e0b
Kirill Sannikov <kir.sannikow@mail.ru>
2019-08-10 00:28:14 +0300

    Создание контроллера и маршрутов для управления постами

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,30 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function () {
-    Route::resource('/posts', 'PostController')->names('blog.posts');
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['namespace' => 'Blog', 'prefix' => '/blog'], function () {
+    Route::resource('/posts', 'PostController',
+        [
+            'names' => 'blog.posts',
+        ],
+    );
+});
+
+//> Админка Блога
+$groupData = [
+    'namespace' => 'Blog\Admin',
+    'prefix'    => '/admin/blog',
+];
+Route::group($groupData, function () {
+    // BlogCategory
+    Route::resource('/categories', 'CategoryController',
+        [
+            'only' => ['index', 'create', 'store', 'edit', 'update'],
+            'names' => 'blog.admin.categories',
+        ],
+    );
 });
 
 // Route::resource('/rest', 'RestTestController')->names('restTest');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
