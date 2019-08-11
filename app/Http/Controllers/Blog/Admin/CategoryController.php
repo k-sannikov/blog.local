a51fa0a0b96637797f75dac13143d8d85268cda9
Kirill Sannikov <kir.sannikow@mail.ru>
2019-08-11 21:13:23 +0300

    Валидация формы редактирования категории

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Blog\Admin;
 
 use App\Models\BlogCategory;
 use Illuminate\Http\Request;
+use App\Http\Requests\Blog\Category\BlogCategoryUpdateRequest;
 
 class CategoryController extends BaseController
 {
@@ -61,7 +62,7 @@ class CategoryController extends BaseController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(BlogCategoryUpdateRequest $request, $id)
     {
         $item = BlogCategory::find($id);
         if (empty($item)) {
