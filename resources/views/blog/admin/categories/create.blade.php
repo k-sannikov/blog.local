8aac137420e7e895d9cda184a7a109bbf2199609
Kirill Sannikov <kir.sannikow@mail.ru>
2019-08-16 13:28:34 +0300

    Переименование и правка шаблонов для работы с категориями (админ)

--- resources/views/blog/admin/categories/create.blade.php
+++ resources/views/blog/admin/categories/create.blade.php
@@ -4,17 +4,15 @@
     <form method="post" action="{{ route('blog.admin.categories.store') }}">
         @csrf
         <div class="container">
-            @include('blog.admin.categories.includes.errors')
-            @include('blog.admin.categories.includes.notifications')
+            @include('blog.admin.categories.includes.result_messages')
             <div class="row justify-content-center">
                 <div class="col-md-8">
-                    @include('blog.admin.categories.includes.item_form_main_col')
+                    @include('blog.admin.categories.includes.category_form_main_col')
                 </div>
                 <div class="col-md-3">
-                    @include('blog.admin.categories.includes.item_form_add_col')
+                    @include('blog.admin.categories.includes.category_form_add_col')
                 </div>
             </div>
         </div>
-
     </form>
 @endsection
--- resources/views/blog/admin/categories/includes/result_messages.blade.php
+++ resources/views/blog/admin/categories/includes/result_messages.blade.php
@@ -1,3 +1,19 @@
+{{-- Вывод списка ошибок --}}
+@if(!is_null($errors->all()))
+    @foreach($errors->all() as $error)
+        <div class="row justify-content-center">
+            <div class="col-md-11">
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    {{ $error }}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    @endforeach
+@endif
+
 {{-- Вывод списка уведомлений --}}
 @if(!is_null(session('message')))
     @foreach(session('message') as $message)
